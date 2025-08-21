ec977be009286d0872e7149056d7a578a79f32db
Divyansh Tripathi <acc4divyansh@gmail.com>
2025-08-22 00:00:01 +0530

    Add files via upload

---
+++ DS-LAB/Assignment2/Additional-Ques/AQ2.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+
+bool canSplitIntoThree(string s) {
+    int n = s.length();
+    for (int len = 1; len < n; len++) {
+        string part = s.substr(0, len);
+        string rest = s.substr(len);
+        bool isSubstring(const string &str, const string &part) {
+    int n = str.size();
+    int m = part.size();
+    
+    for (int i = 0; i <= n - m; i++) {
+        int j;
+        for (j = 0; j < m; j++) {
+            if (str[i + j] != part[j])
+                break;
+        }
+        if (j == m)  
+            return true;
+    }
+    return false;
+}
+
+int main() {
+    string s;
+    cout << "enter string: ";
+    cin >> s;
+
+    if (canSplitIntoThree(s)) {
+        cout << "yes";
+    } else {
+        cout << "no";
+    }
+
+    return 0;
+}
+
+}
+}
+
